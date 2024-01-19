54cf667ca7ab820762da657bda93e2fc55ac8e4a
Eduardo Efrain Santos Arellano <80784706+SYSTEM-OTMX@users.noreply.github.com>
2024-01-18 22:29:50 -0600

    SE AGREGO LOS ID PARA EL USO DE BINDING

--- app/src/main/java/com/system_otmx/calculadora/MainActivity.kt
+++ app/src/main/java/com/system_otmx/calculadora/MainActivity.kt
@@ -2,6 +2,7 @@ package com.system_otmx.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.system_otmx.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.BtnSYSTEMOTMX.setOnClickListener {
+            Toast.makeText(this, "HOLA", Toast.LENGTH_SHORT).show()
+        }
+
+
+        
+        
     }
 }
